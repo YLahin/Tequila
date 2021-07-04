6b7065165f6f9b6f76f3792bcfc578c6237fba16
Yaroslav Lahin <tiarwe@gmail.com>
2021-07-05 02:29:05 +0300

    Update Scopes

---
+++ Package/Scripts/Editor/Scopes/AssetEditingScope.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine.Assertions;
+
+namespace Tequila.Scopes
+{
+    public readonly struct AssetEditingScope : IDisposable
+    {
+        private readonly bool _valid;
+
+        private AssetEditingScope(bool valid)
+        {
+            _valid = valid;
+        }
+
+        public static AssetEditingScope Create()
+        {
+            AssetDatabase.StartAssetEditing();
+
+            return new AssetEditingScope(true);
+        }
+
+        void IDisposable.Dispose()
+        {
+            Assert.IsTrue(_valid);
+            AssetDatabase.StopAssetEditing();
+        }
+    }
+}
